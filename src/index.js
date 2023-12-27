5259578c4f1a6a7b2875b40244e83b69ebcbdb14
Seva <seva.leonov@hotmail.com>
2023-12-27 13:42:33 +0700

    fix bug with cells not updating when dragging away from the board

--- src/index.js
+++ src/index.js
@@ -309,9 +309,11 @@ document.addEventListener("mouseup", (event) => {
       });
     }
 
-    if (elementBelow) {
+    if (elementBelow !== null) {
       makeDroppable(elementBelow, event);
       appended = true;
+    } else if (elementBelow === null) {
+      makeDroppable(originalElementBelow, event);
     }
 
     draggableElement.style.left = 0;
@@ -349,117 +351,53 @@ function makeDroppable(elBelow, event) {
 
     cells.push(firstCell);
 
-    if (draggableElementRect.width >= 80) {
-      const firstCellClassArray = firstCell.classList[0].split(",").map(Number);
-      const parentDivBoardOne = document.querySelector(".playerOne-board");
-      secondCellH = DOMHandler.findDivByCoordinates(
-        `${firstCellClassArray[0]},${firstCellClassArray[1] + 1}`,
-        parentDivBoardOne,
-      );
-      thirdCellH = DOMHandler.findDivByCoordinates(
-        `${firstCellClassArray[0]},${firstCellClassArray[1] + 2}`,
-        parentDivBoardOne,
-      );
-      fourthCellH = DOMHandler.findDivByCoordinates(
-        `${firstCellClassArray[0]},${firstCellClassArray[1] + 3}`,
-        parentDivBoardOne,
-      );
-      secondCellV = DOMHandler.findDivByCoordinates(
-        `${firstCellClassArray[0] + 1},${firstCellClassArray[1]}`,
-        parentDivBoardOne,
-      );
-      thirdCellV = DOMHandler.findDivByCoordinates(
-        `${firstCellClassArray[0] + 2},${firstCellClassArray[1]}`,
-        parentDivBoardOne,
-      );
-      fourthCellV = DOMHandler.findDivByCoordinates(
-        `${firstCellClassArray[0] + 3},${firstCellClassArray[1]}`,
-        parentDivBoardOne,
-      );
-
-      console.log(firstCell);
-      console.log(secondCellH);
-      console.log(thirdCellH);
-      console.log(fourthCellH);
-      console.log(secondCellV);
-      console.log(thirdCellV);
-      console.log(fourthCellV);
-
-      const cellElements = document.elementsFromPoint(
-        event.clientX - mouseDownOffsetHor + 40,
-        event.clientY - mouseDownOffsetVer,
-      );
-      cellElements.forEach((element) => {
-        if (element.classList.contains("cell")) {
-          secondCellH = element;
-          cells.push(secondCellH);
-        }
-      });
-    }
-
-    if (draggableElementRect.width >= 120) {
-      const cellElements = document.elementsFromPoint(
-        event.clientX - mouseDownOffsetHor + 80,
-        event.clientY - mouseDownOffsetVer,
-      );
-      cellElements.forEach((element) => {
-        if (element.classList.contains("cell")) {
-          thirdCellH = element;
-          cells.push(thirdCellH);
-        }
-      });
-    }
+    const firstCellClassArray = firstCell.classList[0].split(",").map(Number);
+    const parentDivBoardOne = document.querySelector(".playerOne-board");
+    secondCellH = DOMHandler.findDivByCoordinates(
+      `${firstCellClassArray[0]},${firstCellClassArray[1] + 1}`,
+      parentDivBoardOne,
+    );
+    thirdCellH = DOMHandler.findDivByCoordinates(
+      `${firstCellClassArray[0]},${firstCellClassArray[1] + 2}`,
+      parentDivBoardOne,
+    );
+    fourthCellH = DOMHandler.findDivByCoordinates(
+      `${firstCellClassArray[0]},${firstCellClassArray[1] + 3}`,
+      parentDivBoardOne,
+    );
+    secondCellV = DOMHandler.findDivByCoordinates(
+      `${firstCellClassArray[0] + 1},${firstCellClassArray[1]}`,
+      parentDivBoardOne,
+    );
+    thirdCellV = DOMHandler.findDivByCoordinates(
+      `${firstCellClassArray[0] + 2},${firstCellClassArray[1]}`,
+      parentDivBoardOne,
+    );
+    fourthCellV = DOMHandler.findDivByCoordinates(
+      `${firstCellClassArray[0] + 3},${firstCellClassArray[1]}`,
+      parentDivBoardOne,
+    );
 
     if (draggableElementRect.width >= 160) {
-      const cellElements = document.elementsFromPoint(
-        event.clientX - mouseDownOffsetHor + 120,
-        event.clientY - mouseDownOffsetVer,
-      );
-      cellElements.forEach((element) => {
-        if (element.classList.contains("cell")) {
-          fourthCellH = element;
-          cells.push(fourthCellH);
-        }
-      });
-    }
-
-    if (draggableElementRect.height >= 80) {
-      const cellElements = document.elementsFromPoint(
-        event.clientX - mouseDownOffsetHor,
-        event.clientY - mouseDownOffsetVer + 40,
-      );
-      cellElements.forEach((element) => {
-        if (element.classList.contains("cell")) {
-          secondCell = element;
-          cells.push(secondCell);
-        }
-      });
-    }
-
-    if (draggableElementRect.height >= 120) {
-      const cellElements = document.elementsFromPoint(
-        event.clientX - mouseDownOffsetHor,
-        event.clientY - mouseDownOffsetVer + 80,
-      );
-      cellElements.forEach((element) => {
-        if (element.classList.contains("cell")) {
-          thirdCell = element;
-          cells.push(thirdCell);
-        }
-      });
+      cells.push(secondCellH);
+      cells.push(thirdCellH);
+      cells.push(fourthCellH);
+    } else if (draggableElementRect.width >= 120) {
+      cells.push(secondCellH);
+      cells.push(thirdCellH);
+    } else if (draggableElementRect.width >= 80) {
+      cells.push(secondCellH);
     }
 
     if (draggableElementRect.height >= 160) {
-      const cellElements = document.elementsFromPoint(
-        event.clientX - mouseDownOffsetHor,
-        event.clientY - mouseDownOffsetVer + 120,
-      );
-      cellElements.forEach((element) => {
-        if (element.classList.contains("cell")) {
-          fourthCell = element;
-          cells.push(fourthCell);
-        }
-      });
+      cells.push(secondCellV);
+      cells.push(thirdCellV);
+      cells.push(fourthCellV);
+    } else if (draggableElementRect.height >= 120) {
+      cells.push(secondCellV);
+      cells.push(thirdCellV);
+    } else if (draggableElementRect.height >= 80) {
+      cells.push(secondCellV);
     }
 
     // Set droppable attribute on the cells under draggable element
@@ -473,7 +411,6 @@ function makeDroppable(elBelow, event) {
 
     // Update the graph accordingly
     cellsVertices.forEach((cellVertex) => {
-      console.log(cellVertex);
       cellVertex.occupiedByShip = true;
       cellVertex.occupied = true;
 
