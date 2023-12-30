e8b7b837d3cea8d105bfc36063006d69fbc80294
Seva <seva.leonov@hotmail.com>
2023-12-30 11:17:38 +0700

    add players to the board

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable import/no-mutable-exports */
 import "./styles.css";
 // eslint-disable-next-line import/no-cycle
 import DOMHandler from "./DOMHandler";
 import GameboardModule from "./gameboard";
 import { addDocumentEventListeners } from "./eventManager";
+import Player from "./player";
 
 let playerOneBoard = null;
 let playerTwoBoard = null;
+let playerOne = null;
+let playerTwo = null;
+let currentPlayer = null;
 
 function gameLoop() {
   const main = document.querySelector(".main");
@@ -17,12 +22,17 @@ function gameLoop() {
   playerOneBoard.createShips();
   playerOneBoard.positionShips();
   playerOneBoard.locked = false;
+  playerOne = Player(playerOneBoard, "human");
+  playerOneBoard.player = playerOne;
+  currentPlayer = playerOne;
 
   playerTwoBoard = GameboardModule();
   playerTwoBoard.createBoard();
   playerTwoBoard.createShips();
   playerTwoBoard.positionShips();
   playerTwoBoard.locked = true;
+  playerTwo = Player(playerTwoBoard, "computer");
+  playerTwoBoard.player = playerTwo;
 
   DOMHandler.playerOneBoard = playerOneBoard.board.vertices;
   DOMHandler.playerOneShips = playerOneBoard.ships;
@@ -38,8 +48,8 @@ addDocumentEventListeners();
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "c" || event.key === "C") {
-    console.log(playerOneBoard);
-    console.log(playerTwoBoard);
+    console.log(playerOne);
+    console.log(playerTwo);
   }
 });
 
